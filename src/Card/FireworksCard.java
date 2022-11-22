87d45502f0eef947fd25dbb0cfb0a801425463ea
HanYang <YangHan991123@gmail.com>
2022-11-22 10:57:34 +0100

    Done with Fireworks and Cloverleaf card

--- src/Card/FireworksCard.java
+++ src/Card/FireworksCard.java
@@ -1,16 +1,46 @@
 package Card;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 //Composite design pattern - leaf
+//Keep throwing dice until null
 public class FireworksCard extends Card{
     public FireworksCard(Optional<Suit> suit) {
         super(suit);
     }
     @Override
     public Optional<Integer> playGame() {
-
+        int playPoints=0;
         super.continuousAfterTutto=false;
-        return Optional.ofNullable(0);
+        Optional<int[]> dice = diceTool.generateDice(6);
+        //for(int i =0; i<6;i++) diceCopy[i] = dice.get()[i];
+        //If contains at least one valid dice--------------------------------------------------------
+        if(diceTool.isValidate(dice.get())){
+            // Cannot stop until NULL
+            System.out.println("You cannot stop before NULL.");
+            while(dice.isPresent()){
+                List<int[]> allValidDiceChoice = diceTool.allValidDice(dice.get());
+                int[] allValidDice = allValidDiceChoice.get(allValidDiceChoice.size());
+                // Roll the remaining dice and keep all valid dice
+                dice = super.remainingDice(dice,allValidDice);
+                playPoints = playPoints + diceTool.calculatePoints(allValidDice);
+                if(dice.isEmpty()){ //Tutto and continue throwing dice
+                    System.out.println("TUTTO! You cannot stop before NULL.");
+                    dice = diceTool.generateDice(6);
+                    if(!diceTool.isValidate(dice.get())){ //If no valid dice
+                        break;
+                    }
+                }
+            }
+            System.out.println("You have rolled a null. Next turn.");
+            return Optional.ofNullable(playPoints);
+            // No valid dice ------------------------------------------------------------------------------------------
+        }else{
+            super.continuousAfterTutto = false;
+            System.out.println("You have rolled a null. Next turn.");
+            return Optional.empty();
+        }
     }
 }

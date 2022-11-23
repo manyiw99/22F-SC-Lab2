bb70f224bd43a2799690e32a88c0f4fe5c134605
Duanran Jing <duanran.jing@outlook.com>
2022-11-23 18:14:54 +0100

    AllValidDice and validation for straight card are needed

--- src/Tools/DiceCalculation.java
+++ src/Tools/DiceCalculation.java
@@ -29,7 +29,10 @@ public class DiceCalculation {
      * @return
      */
     public static boolean isValidate(int[] dice){
-        //只要不是Null就返回true
+        //return true as long as the dice is not null
+        if (dice == null) {
+            return false;
+        }
         return true;
     }
 
@@ -39,10 +42,21 @@ public class DiceCalculation {
      * @return
      */
     public static List<int[]> allValidDice(int[] dice){
+        // todo: find out all the combination of valid dices
+        // problem: five and one should be less than 3
         List<int[]> result = new ArrayList<>();
-        result.add(new int[]{3});
+        int[] counter = count(dice);
+
 
         return result;
+    }
+
+    public static int[] count(int[] dice) {
+        var counter = new int[7];
+        for (int i = 0; i < dice.length; i++) {
+            counter[dice[i]] += 1;
+        }
+        return counter;
     }
 
     /**
@@ -50,10 +64,79 @@ public class DiceCalculation {
      * @param dice
      * @return
      */
-    public static int calculatePoints(int[] dice){
-        // 最后返回的是输入的骰子可以获得的最大points，dice有可能是六位也有可能是12345位
-        return 100;
+    public static int calculateSingleDices(int[] dice, int result) {
+        for (int i = 0; i < dice.length; i++) {
+            if (dice[i] == 1) {
+                result += 100;
+            } else {
+                result += 50;
+            }
+            dice[i] = 0;
+        }
+        return  result;
     }
+
+    public static int calculateThreeDices(int[] dice, int result) {
+        var counter = count(dice);
+
+        // find some value that occurs three times and replace it by 0
+        int j = 0, found = 0, value = -1;
+        for (; j < dice.length; j++) {
+            if (counter[dice[j]] >= 3) {
+                value = dice[j];
+                dice[j] = 0;
+                found = 1;
+                break;
+            }
+        }
+
+        if (value == -1) {
+            System.out.println("no value occurs three times");
+            return -1;
+        };
+
+        // replace two other occurrences of value in dice with 0
+        for (; j < dice.length && found < 3; j++) {
+            if (dice[j] == value) {
+                dice[j] = 0;
+                found++;
+            }
+        }
+        return result + ((value == 1) ? 1000 : value * 100);
+    }
+
+    public static boolean exitThreeDices(int[] dice) {
+        int[] counter = count(dice);
+        for (int i = 1; i < 8; i++) {
+            if (counter[i] == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static int calculatePoints(int[] dice){
+        int len = dice.length, point = 0;
+        if (len == 1 || len == 2) {
+            point = calculateSingleDices(dice, point);
+        } else {
+            int sum = Arrays.stream(dice).sum();
+            while (sum != 0) {
+                boolean existThree = exitThreeDices(dice);
+                if (existThree) {
+                    calculateThreeDices(dice, point);
+                } else {
+                    calculateSingleDices(dice, point);
+                }
+                sum = Arrays.stream(dice).sum();
+            }
+        }
+            return point;
+
+
+        }
+        // 最后返回的是输入的骰子可以获得的最大points，dice有可能是六位也有可能是12345位
+
+
 
     public static int[] selectDice(List<int[]> allValidDice){
         int[] selectedDice = null;

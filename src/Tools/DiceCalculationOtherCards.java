2c5d95a94a50c47c31fca3208763c8016a0605c6
wenzli0510 <wenzhe.li@uzh.ch>
2022-11-28 21:47:13 +0100

    fix bug after generating dice

--- src/Tools/DiceCalculationOtherCards.java
+++ src/Tools/DiceCalculationOtherCards.java
@@ -9,12 +9,120 @@ import static Tools.InputValidation.readUser;
 import static Tools.InputValidation.validateSelectNum;
 
 public interface DiceCalculationOtherCards extends DiceCalculationAllCards{
+
+    static List<int[]> allValidDice(int[] dice, int k) {
+        List<Integer> pos = allValidValue(dice);
+        int[] choice = pos.stream().mapToInt(i -> i).toArray();
+        List<int[]> result = combination(choice, k);
+        return result;
+    }
+
+    static int[] selectDice(List<int[]> allValidDice) {
+        int[] selectedDice = null;
+
+        boolean isSelected = false;
+        while (!isSelected) {
+            System.out.println("Please choose the valid dice you want to keep(enter the number): ");
+            for (int a = 0; a < allValidDice.size(); a++) {
+                System.out.println("(" + (a + 1) + ")  " + Arrays.toString(allValidDice.get(a)));
+            }
+            String selectedInput = readUser();
+            if (validateSelectNum(selectedInput, allValidDice.size())) {
+                selectedDice = allValidDice.get(Integer.parseInt(selectedInput) - 1);
+                isSelected = true;
+            } else {
+                System.out.println("Your selection is wrong, please enter again.");
+            }
+        }
+
+        return selectedDice;
+    }
+
     static int[] count(int[] dice) {
         var counter = new int[7];
         for (int i = 0; i < dice.length; i++) {
             counter[dice[i]] += 1;
         }
         return counter;
+    }
+
+    /**
+     * Return all possible combinations of valid dice
+     *
+     * @param dice
+     * @return
+     */
+
+    static List<Integer> allValidValue(int[] dice) {
+        List<Integer> pos = new ArrayList<Integer>();
+        int[] counter = DiceCalculationOtherCards.count(dice);
+        for (int i = 1; i < 7; i++) {
+            if (((i != 1) && counter[i] < 3) || ((i != 5) && counter[i] < 3) && counter[i] == 0) {
+                continue;
+            } else {
+                int size = counter[i];
+                for (int j = 0; j < size; j++) {
+                    pos.add(i);
+                }
+            }
+        }
+        return pos;
+    }
+
+    static boolean selectedSizeValidation(int[] dice, int k){
+        List<Integer> pos = allValidValue(dice);
+        if (k > pos.size()) {
+            return false;
+        }
+        return true;
+    }
+
+    static List<int[]> combination(int[] e, int k){
+        List<int[]> result = new ArrayList<>();
+        int[] ignore = new int[e.length-k]; // --> [0][0]
+        int[] combination = new int[k]; // --> [][][]
+
+        // set initial ignored elements
+        //(last k elements will be ignored)
+        for(int w = 0; w < ignore.length; w++)
+            ignore[w] = e.length - k +(w+1);
+
+        int i = 0, r = 0, g = 0;
+
+        boolean terminate = false;
+        while(!terminate){
+
+            // selecting N-k non-ignored elements
+            while(i < e.length && r < k){
+
+                if(i != ignore[g]){
+                    combination[r] = i;
+                    r++;
+                }
+                else{
+                    if(g != ignore.length-1)
+                        g++;
+                }
+                i++;
+            }
+            result.add(combination);
+            i = 0; r = 0; g = 0;
+
+            terminate = true;
+
+            // shifting ignored indices
+            for(int w = 0 ; w < ignore.length; w++){
+                if(ignore[w] > w){
+                    ignore[w]--;
+
+                    if(w > 0)
+                        ignore[w-1] = ignore[w]-1;
+                    terminate = false;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 
     /**
@@ -110,47 +218,5 @@ public interface DiceCalculationOtherCards extends DiceCalculationAllCards{
         return true;
     }
 
-    /**
-     * Return all possible combinations of valid dice
-     *
-     * @param dice
-     * @return
-     */
-    static List<int[]> allValidDice(int[] dice) {
-        // todo: find out all the combination of valid dices
-        // problem: five and one should be less than 3
-        List<int[]> result = new ArrayList<>();
-        int[] counter = DiceCalculationOtherCards.count(dice);
 
-<<<<<<< HEAD
-
-        return result;
-    }
-
-
-    static int[] selectDice(List<int[]> allValidDice) {
-        int[] selectedDice = null;
-
-        boolean isSelected = false;
-        while (!isSelected) {
-            System.out.println("Please choose the valid dice you want to keep(enter the number): ");
-            for (int a = 0; a < allValidDice.size(); a++) {
-                System.out.println("(" + (a + 1) + ")  " + Arrays.toString(allValidDice.get(a)));
-            }
-            String selectedInput = readUser();
-            if (validateSelectNum(selectedInput, allValidDice.size())) {
-                selectedDice = allValidDice.get(Integer.parseInt(selectedInput) - 1);
-                isSelected = true;
-            } else {
-                System.out.println("Your selection is wrong, please enter again.");
-            }
-        }
-
-        return selectedDice;
-    }
-=======
-    List<int[]> allValidDice(int[] dice, int k);
-
-    int[] selectDice(List<int[]> allValidDice);
->>>>>>> 681cf2c2e0df15ddb1aa1a80c75c8be350a5b21d
 }

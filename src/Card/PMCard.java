db88efb363d532b244d84e4a28162bed6e29414f
manyiw99 <1762019598@qq.com>
2022-11-30 13:02:59 +0100

    Fix PM: continue after invalid dice

--- src/Card/PMCard.java
+++ src/Card/PMCard.java
@@ -19,27 +19,29 @@ public class PMCard extends Card{
     public Optional<Integer> playGame() {
         Optional<int[]> dice = DiceCalculationAllCards.generateDice(6);
         //If contains at least one valid dice--------------------------------------------------------
-        if(DiceCalculationOtherCards.isValidate(dice)){
+
             // Cannot stop until TUTTO
             System.out.println("You cannot stop before TUTTO.");
-            while(dice.isPresent()){
-                // Get the dice to keep-----------------------------------------
-                List<int[]> selectedDice = DiceCalculationOtherCards.selectDice(dice.get());
+            while(dice.isPresent()) {
+                if (DiceCalculationOtherCards.isValidate(dice)) {
+                    // Get the dice to keep-----------------------------------------
+                    List<int[]> selectedDice = DiceCalculationOtherCards.selectDice(dice.get());
 
-                int selectedDiceLength=0;
-                for (int l = 0; l < selectedDice.size(); l++) {
-                    selectedDiceLength = selectedDice.get(l).length + selectedDiceLength;
+                    int selectedDiceLength = 0;
+                    for (int l = 0; l < selectedDice.size(); l++) {
+                        selectedDiceLength = selectedDice.get(l).length + selectedDiceLength;
+                    }
+                    // Roll the remaining dice
+                    dice = super.remainingDice(dice, selectedDiceLength);
+                    // No valid dice ------------------------------------------------------------------------------------------
+                }else{
+                    super.continuousAfterTutto = false;
+                    System.out.println("You have rolled a null. Next turn.");
+                    return Optional.empty();
                 }
-                // Roll the remaining dice
-                dice = super.remainingDice(dice,selectedDiceLength);
             }
 
             return Optional.ofNullable(1000);
-        // No valid dice ------------------------------------------------------------------------------------------
-        }else{
-            super.continuousAfterTutto = false;
-            System.out.println("You have rolled a null. Next turn.");
-            return Optional.empty();
-        }
+
     }
 }

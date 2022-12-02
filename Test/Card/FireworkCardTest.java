784f60356b47d8cc26e94e76c5e21a64e547129f
wenzli0510 <wenzhe.li@uzh.ch>
2022-12-03 00:24:03 +0100

    fix firework test

--- Test/Card/FireworkCardTest.java
+++ Test/Card/FireworkCardTest.java
@@ -32,7 +32,7 @@ public class FireworkCardTest {
     }
 
     @Test
-    public void noValidDice() {
+    public void noValidDiceTest() {
 
         int[] dice = {1, 1, 1, 5, 5, 5};
         Optional<int[]> tuttoDice = Optional.ofNullable(dice);
@@ -41,6 +41,65 @@ public class FireworkCardTest {
         when(diceTool.generateDice(6)).thenReturn(tuttoDice);
         when(diceTool.isValidate(Mockito.any())).thenReturn(true);
         when(diceTool.allValidValue(Mockito.any())).thenReturn(diceList);
+
+        Optional<Integer> gamePoints = fireworksCard.playGame();
+        Assertions.assertEquals(Optional.ofNullable(0), gamePoints);
+    }
+
+    @Test
+    public void noDiceTest() {
+        when(diceTool.generateDice(6)).thenReturn(Optional.empty());
+        when(diceTool.isValidate(Mockito.any())).thenReturn(true);
+
+        Assertions.assertEquals(Optional.ofNullable(0), fireworksCard.playGame());
+    }
+
+    @Test
+    public void tuttoThenNoValidDiceTest() {
+
+        int[] dice = {1, 1, 1, 5, 5, 5};
+        Optional<int[]> tuttoDice = Optional.ofNullable(dice);
+        List<Integer> diceList = List.of(1, 1, 1, 5, 5, 5);
+
+        when(diceTool.generateDice(6)).thenReturn(tuttoDice).thenReturn(Optional.empty());
+        when(diceTool.isValidate(Mockito.any())).thenReturn(true).thenReturn(false);
+        when(diceTool.allValidValue(Mockito.any())).thenReturn(diceList);
+        when(diceTool.calculatePoints(dice)).thenReturn(1000);
+
+        Optional<Integer> gamePoints = fireworksCard.playGame();
+        Assertions.assertEquals(Optional.ofNullable(1000), gamePoints);
+    }
+
+    @Test
+    public void tuttoThenValidDiceTest() {
+
+        int[] dice = {1, 1, 1, 5, 5, 5};
+        Optional<int[]> tuttoDice = Optional.ofNullable(dice);
+        List<Integer> diceList = List.of(1, 1, 1, 5, 5, 5);
+
+        when(diceTool.generateDice(6)).thenReturn(tuttoDice).thenReturn(Optional.empty());
+        when(diceTool.isValidate(Mockito.any())).thenReturn(true).thenReturn(true);
+        when(diceTool.allValidValue(Mockito.any())).thenReturn(diceList);
+        when(diceTool.calculatePoints(dice)).thenReturn(1000);
+
+        Optional<Integer> gamePoints = fireworksCard.playGame();
+        Assertions.assertEquals(Optional.ofNullable(1000), gamePoints);
+    }
+
+    @Test
+    public void noTuttoAtFirstTest() {
+
+        int[] dice = {1, 1, 1, 5, 5, 5};
+        Optional<int[]> tuttoDice = Optional.ofNullable(dice);
+        List<Integer> diceList = List.of(1, 1, 1, 5, 5);
+        List<Integer> diceList2 = new ArrayList<>();
+        int[] dice1 = {1};
+        Optional<int[]> dice2 = Optional.ofNullable(dice1);
+
+        when(diceTool.generateDice(6)).thenReturn(tuttoDice).thenReturn(dice2);
+        when(diceTool.isValidate(Mockito.any())).thenReturn(true);
+        when(diceTool.allValidValue(Mockito.any())).thenReturn(diceList).thenReturn(diceList2);
+        when(diceTool.calculatePoints(dice)).thenReturn(1000);
 
         Optional<Integer> gamePoints = fireworksCard.playGame();
         Assertions.assertEquals(Optional.ofNullable(0), gamePoints);
